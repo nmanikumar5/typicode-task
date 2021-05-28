d6cb3cffd1e72eb8081913ffa0f38971b0a708c9
ManiKumar Navara <nmanikumar5@gmail.com>
2021-05-28 16:38:14 +0400

    Removed Unused consts

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,11 +3,6 @@ export const API_URL = "https://jsonplaceholder.typicode.com/";
 export const API_SUCCESS_CODE = 200;
 export const API_C_SUCCESS_CODE = 201;
 
-export const H_CONSTANTS = {
-    SEARCH: "SEARCH",
-    ALBUMS: "ALBUMS",
-};
-
 export const H_LABELS = {
     ADD_POST: "ADD POST",
 };
@@ -16,8 +11,6 @@ export const COLOR_CODES = {
     primary: '#3E47FF',
     darkGray: "#282828",
     white: "#FFFFFF",
-    border: "#515151",
-    lightGray: "#a1a1a1",
     black: "#000000",
     green: "#1f7039"
 };
